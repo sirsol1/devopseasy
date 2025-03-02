eb5dff75fa96dec186c1b8beba1f032afbb17836
Devops Easy Learning <96950933+devopseasylearning@users.noreply.github.com>
2025-03-02 12:43:33 -0600

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,31 @@
+docker run -d --name database \
+  --restart always \
+  -e MYSQL_ROOT_PASSWORD=abc123 \
+  -e MYSQL_DATABASE=openSIS \
+  -e MYSQL_USER=openSIS_rw \
+  -e MYSQL_PASSWORD=Op3nS!S \
+  -v db_data:/var/lib/mysql \
+  -v $(pwd)/mysql-init:/docker-entrypoint-initdb.d \
+  -v $(pwd)/mysql-config/strict_mode.cnf:/etc/mysql/conf.d/strict_mode.cnf \
+  --network opensis_network \
+  --network-alias opensis \
+  mariadb:10.5
+
+
+
+
+docker run -d --name frontend \
+  -p 8080:80 -p 80:80 \
+  -v openSIS:/var/www/html \
+  --network opensis_network \
+  --network-alias opensis \
+  --depends-on database \
+  your-frontend-image
+
+
+docker network create opensis_network
+docker volume create db_data
+docker volume create openSIS
+
+
+docker build -t your-frontend-image .
